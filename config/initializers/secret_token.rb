b89e0cbcc257314cd6b541b44ac65a254bd2681e
cenbek <sa.ordekci@gmail.com>
2013-02-22 13:49:28 +0200

    project initialized was made.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omniidentity::Application.config.secret_token = 'f40438baf125c3416842be1872a87afc696cfd7920225067e7a86e5b650f62b83e1b584f2aba1e10c54e5c8dc48ff69580ed4e5b1ade3abf9424d8bfa4538093'
