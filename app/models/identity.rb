9c6b987d966c1dc553900a284cca6ece5565d8c3
cenbek <sa.ordekci@gmail.com>
2013-02-22 14:50:47 +0200

    sessions controller was created

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -5,4 +5,9 @@ class Identity
   field :name, type: String
   field :email, type: String
   field :password_digest, type: String
+  
+  validates_presence_of :name
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  
 end
